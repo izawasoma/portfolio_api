37a8d502c6504eddafae938d38da60051281f493
“izawasoma” <izawasoma@gmail.com>
2025-01-03 19:57:36 +0900

    リクエストテスト用の機能を追加

---
+++ app/Http/Requests/SampleRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class SampleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "title" => "required|string|max:8",
+            "memo" => "string|max:20",
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response['errors']  = $validator->errors()->toArray();
+        throw new HttpResponseException(response()->json($response, Response::HTTP_BAD_REQUEST));
+    }
+}
---
+++ database/factories/SampleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sample>
+ */
+class SampleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title(8),
+            'memo' => fake()->realText(20),
+        ];
+    }
+}

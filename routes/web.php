eedca3b1050243bce62f72763d6382dd103422cb
“izawasoma” <izawasoma@gmail.com>
2025-01-05 17:34:38 +0900

    ファイルアップロード機能完成

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,9 @@ Route::group(["prefix" => "sample", "as" => "sample."], function() {
     Route::get("go-add", [\App\Http\Controllers\SampleController::class, "goAdd"])->name("goAdd");
     Route::post("", [\App\Http\Controllers\SampleController::class, "add"])->name("add");
 });
+
+Route::group(["prefix" => "file", "as" => "file."], function() {
+    Route::get("", [\App\Http\Controllers\FileController::class, "list"])->name("list");
+    Route::get("go-add", [\App\Http\Controllers\FileController::class, "goAdd"])->name("goAdd");
+    Route::post("", [\App\Http\Controllers\FileController::class, "add"])->name("add");
+});
